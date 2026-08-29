9608b419ab987e243788aa217977bb0414fc061d
nemo159 <rnrtjd1002@naver.com>
2026-08-29 15:18:43 +0900

    멤버등록 서비스 init

---
+++ services/member-service/src/test/java/com/study/apim/memberservice/MemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.apim.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
